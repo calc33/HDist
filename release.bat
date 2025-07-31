52f0cabbb18591c44c65f896430a11c9b1efc3c9
BABA Motoharu <calc33@gmail.com>
2025-07-31 19:09:22 +0900

    リリース用バッチの修正

--- release.bat
+++ release.bat
@@ -2,7 +2,6 @@
 
 MKDIR Release
 
-XCOPY /S /Y /I /EXCLUDE:ignore.txt HDistCore\bin\Release\net5.0 Release
-XCOPY /S /Y /I /EXCLUDE:ignore.txt BuildSum\bin\Release\net5.0 Release
-XCOPY /S /Y /I /EXCLUDE:ignore.txt HCopy\bin\Release\net5.0 Release
-XCOPY /S /Y /I /EXCLUDE:ignore.txt HCopyW\bin\Release\net5.0-windows Release
+XCOPY /S /Y /I /EXCLUDE:ignore.txt BuildSum\bin\Release\net9.0 Release
+XCOPY /S /Y /I /EXCLUDE:ignore.txt HCopy\bin\Release\net9.0 Release
+XCOPY /S /Y /I /EXCLUDE:ignore.txt HCopyW\bin\Release\net9.0-windows10.0.22000.0 Release
